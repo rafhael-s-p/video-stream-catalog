e8535eb64f44c26e0abf8159c0b377b339b88beb
Rafhael <rafhael.dev@gmail.com>
2024-08-28 14:22:26 -0300

    feat: category aggregate validation

---
+++ domain/src/main/java/com/studies/catalog/domain/exceptions/NotificationException.java
@@ -0,0 +1,10 @@
+package com.studies.catalog.domain.exceptions;
+
+import com.studies.catalog.domain.validation.handler.Notification;
+
+public class NotificationException extends DomainException {
+
+    public NotificationException(final String aMessage, final Notification notification) {
+        super(aMessage, notification.getErrors());
+    }
+}
---
+++ domain/src/main/java/com/studies/catalog/domain/validation/Error.java
@@ -0,0 +1,4 @@
+package com.studies.catalog.domain.validation;
+
+public record Error(String message) {
+}
