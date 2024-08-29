c79b194df0ef31c517a49c8544708d75cbf74e4e
Rafhael <rafhael.dev@gmail.com>
2024-08-29 09:37:11 -0300

    test: category persistence use case

--- application/src/test/java/com/studies/catalog/application/UseCaseTest.java
+++ application/src/test/java/com/studies/catalog/application/UseCaseTest.java
@@ -1,23 +1,10 @@
 package com.studies.catalog.application;
 
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.List;
 
 @Tag("unitTest")
 @ExtendWith(MockitoExtension.class)
-public abstract class UseCaseTest implements BeforeEachCallback {
-
-    @Override
-    public void beforeEach(final ExtensionContext context) {
-        Mockito.reset(getMocks().toArray());
-    }
-
-    protected abstract List<Object> getMocks();
-
+public abstract class UseCaseTest {
 }
