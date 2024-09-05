4c4733fd7b40bd12dbecc23b38a0f8129a13cb16
Rafhael <rafhael.dev@gmail.com>
2024-09-05 10:42:46 -0300

    feat: category graphql query

---
+++ infrastructure/src/main/java/com/studies/catalog/infrastructure/graphql/CategoryGraphQLController.java
@@ -0,0 +1,36 @@
+package com.studies.catalog.infrastructure.graphql;
+
+import com.studies.catalog.application.category.list.ListCategoryOutput;
+import com.studies.catalog.application.category.list.ListCategoryUseCase;
+import com.studies.catalog.domain.category.CategorySearchQuery;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Objects;
+
+@Controller
+public class CategoryGraphQLController {
+
+    private final ListCategoryUseCase listCategoryUseCase;
+
+    public CategoryGraphQLController(final ListCategoryUseCase listCategoryUseCase) {
+        this.listCategoryUseCase = Objects.requireNonNull(listCategoryUseCase);
+    }
+
+    @QueryMapping
+    public List<ListCategoryOutput> categories(
+            @Argument final String search,
+            @Argument final int page,
+            @Argument final int perPage,
+            @Argument final String sort,
+            @Argument final String direction
+    ) {
+
+        final var aQuery =
+                new CategorySearchQuery(page, perPage, search, sort, direction);
+
+        return this.listCategoryUseCase.execute(aQuery).data();
+    }
+}
