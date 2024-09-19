d83db0581ff96438da33488135ea9d2fcb9943bf
Rafhael <rafhael.dev@gmail.com>
2024-09-19 10:10:21 -0300

    test: elasticsearch integrated testing configuration with testcontainers

---
+++ infrastructure/src/test/java/com/studies/catalog/infrastructure/category/CategoryElasticsearchGatewayTest.java
@@ -0,0 +1,17 @@
+package com.studies.catalog.infrastructure.category;
+
+import com.studies.catalog.AbstractElasticsearchTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CategoryElasticsearchGatewayTest extends AbstractElasticsearchTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void testInjection() {
+        Assertions.assertNotNull(categoryRepository);
+    }
+}
