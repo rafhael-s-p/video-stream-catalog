342af223765d2a1c6e84f1cd3cb5cbea352d7f13
Rafhael <rafhael.dev@gmail.com>
2024-09-20 17:03:19 -0300

    feat: category document

--- infrastructure/src/main/java/com/studies/catalog/infrastructure/category/CategoryRepository.java
+++ infrastructure/src/main/java/com/studies/catalog/infrastructure/category/CategoryRepository.java
@@ -1,6 +1,6 @@
 package com.studies.catalog.infrastructure.category;
 
-import com.studies.catalog.infrastructure.CategoryDocument;
+import com.studies.catalog.infrastructure.category.persistence.CategoryDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface CategoryRepository extends ElasticsearchRepository<CategoryDocument, String> {
