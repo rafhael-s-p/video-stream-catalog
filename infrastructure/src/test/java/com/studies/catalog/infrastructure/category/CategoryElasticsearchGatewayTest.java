b79915d96a929ba07d3c4d6dc76b47cd796ef899
Rafhael <rafhael.dev@gmail.com>
2024-09-23 11:28:22 -0300

    test: category gateway saving

--- infrastructure/src/test/java/com/studies/catalog/infrastructure/category/CategoryElasticsearchGatewayTest.java
+++ infrastructure/src/test/java/com/studies/catalog/infrastructure/category/CategoryElasticsearchGatewayTest.java
@@ -1,18 +1,44 @@
 package com.studies.catalog.infrastructure.category;
 
 import com.studies.catalog.AbstractElasticsearchTest;
+import com.studies.catalog.domain.Fixture;
 import com.studies.catalog.infrastructure.category.persistence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class CategoryElasticsearchGatewayTest extends AbstractElasticsearchTest {
+class CategoryElasticsearchGatewayTest extends AbstractElasticsearchTest {
+
+    @Autowired
+    private CategoryElasticsearchGateway categoryGateway;
 
     @Autowired
     private CategoryRepository categoryRepository;
 
     @Test
-    public void testInjection() {
+    void testInjection() {
         Assertions.assertNotNull(categoryRepository);
+    }
+
+    @Test
+    void givenValidCategory_whenCallsSave_shouldPersistIt() {
+        // given
+        final var movies = Fixture.Categories.movies();
+
+        // when
+        final var currentOutput = this.categoryGateway.save(movies);
+
+        // then
+        Assertions.assertEquals(movies, currentOutput);
+
+        final var currentCategory = categoryRepository.findById(movies.id()).get();
+
+        Assertions.assertEquals(movies.id(), currentCategory.id());
+        Assertions.assertEquals(movies.name(), currentCategory.name());
+        Assertions.assertEquals(movies.description(), currentCategory.description());
+        Assertions.assertEquals(movies.active(), currentCategory.active());
+        Assertions.assertEquals(movies.createdAt(), currentCategory.createdAt());
+        Assertions.assertEquals(movies.updatedAt(), currentCategory.updatedAt());
+        Assertions.assertEquals(movies.deletedAt(), currentCategory.deletedAt());
     }
 }
