30e790945f3c6a9f49f8577e4420d1b408d15b80
Rafhael <rafhael.dev@gmail.com>
2024-09-24 09:51:02 -0300

    test: category gateway delete

--- infrastructure/src/test/java/com/studies/catalog/infrastructure/category/CategoryElasticsearchGatewayTest.java
+++ infrastructure/src/test/java/com/studies/catalog/infrastructure/category/CategoryElasticsearchGatewayTest.java
@@ -2,6 +2,7 @@ package com.studies.catalog.infrastructure.category;
 
 import com.studies.catalog.AbstractElasticsearchTest;
 import com.studies.catalog.domain.Fixture;
+import com.studies.catalog.infrastructure.category.persistence.CategoryDocument;
 import com.studies.catalog.infrastructure.category.persistence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,7 +18,8 @@ class CategoryElasticsearchGatewayTest extends AbstractElasticsearchTest {
 
     @Test
     void testInjection() {
-        Assertions.assertNotNull(categoryRepository);
+        Assertions.assertNotNull(this.categoryGateway);
+        Assertions.assertNotNull(this.categoryRepository);
     }
 
     @Test
@@ -31,7 +33,7 @@ class CategoryElasticsearchGatewayTest extends AbstractElasticsearchTest {
         // then
         Assertions.assertEquals(movies, currentOutput);
 
-        final var currentCategory = categoryRepository.findById(movies.id()).get();
+        final var currentCategory = this.categoryRepository.findById(movies.id()).get();
 
         Assertions.assertEquals(movies.id(), currentCategory.id());
         Assertions.assertEquals(movies.name(), currentCategory.name());
@@ -41,4 +43,31 @@ class CategoryElasticsearchGatewayTest extends AbstractElasticsearchTest {
         Assertions.assertEquals(movies.updatedAt(), currentCategory.updatedAt());
         Assertions.assertEquals(movies.deletedAt(), currentCategory.deletedAt());
     }
+
+    @Test
+    void givenValidId_whenCallsDeleteById_shouldDeleteIt() {
+        // given
+        final var movies = Fixture.Categories.movies();
+
+        this.categoryRepository.save(CategoryDocument.from(movies));
+
+        final var expectedId = movies.id();
+        Assertions.assertTrue(this.categoryRepository.existsById(expectedId));
+
+        // when
+        this.categoryGateway.deleteById(expectedId);
+
+        // then
+        Assertions.assertFalse(this.categoryRepository.existsById(expectedId));
+    }
+
+    @Test
+    void givenInvalidId_whenCallsDeleteById_shouldBeOk() {
+        // given
+        final var expectedId = "any";
+
+        // when/then
+        Assertions.assertDoesNotThrow(() -> this.categoryGateway.deleteById(expectedId));
+    }
+
 }
