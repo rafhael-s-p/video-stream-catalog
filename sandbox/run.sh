7cec9c9500c85ac9559f67b60a87c67c137994b6
Rafhael <rafhael.dev@gmail.com>
2024-09-26 18:40:31 -0300

    ci: kafka connect docker

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -5,6 +5,8 @@ docker network create kafka
 
 docker volume create es01
 docker volume create kafka01
+docker volume create kconnect01
 
 docker compose -f elk/docker-compose.yml up -d elasticsearch
 docker compose -f kafka/docker-compose.yml up -d
+docker compose -f catalog_services/docker-compose.yml up -d
