d367738d695cc6b1e556e2faa485995e335d9f1c
Rafhael <rafhael.dev@gmail.com>
2024-10-09 13:30:57 -0300

    feat: fault tolerance bulkhead pattern

--- infrastructure/src/main/java/com/studies/catalog/infrastructure/category/CategoryRestClient.java
+++ infrastructure/src/main/java/com/studies/catalog/infrastructure/category/CategoryRestClient.java
@@ -2,6 +2,7 @@ package com.studies.catalog.infrastructure.category;
 
 import com.studies.catalog.infrastructure.category.models.CategoryDTO;
 import com.studies.catalog.infrastructure.utils.HttpClient;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -24,6 +25,7 @@ public class CategoryRestClient implements HttpClient {
         return NAMESPACE;
     }
 
+    @Bulkhead(name = NAMESPACE)
     @Retry(name = NAMESPACE)
     public Optional<CategoryDTO> getById(final String categoryId) {
         return doGet(categoryId, () ->
